6201d02a88ac154120e1d866457c0a91d7fe7e42
Derek Lee <dereklee@downtothis.com>
2019-08-30 15:22:06 +0900

    Extracts domain logic to separate file + first history test structure

---
+++ rps/spec/historySpec.js
@@ -0,0 +1,9 @@
+describe('history', () => {
+    describe('when no rounds have been played', () => {
+        it('tells the ui that no rounds have been played', () => {
+            // 準備
+            // 実行
+            // 期待
+        })
+    })
+})
--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,45 +1,4 @@
-function Requests() {
-    this.play = (p1Throw, p2Throw, observer) => {
-        new PlayRoundRequest(p1Throw, p2Throw, observer).process()
-    }
-}
-
-function PlayRoundRequest(p1Throw, p2Throw, observer) {
-    this.process = () => {
-        if (isInvalidThrow(p1Throw) ||
-            isInvalidThrow(p2Throw)) {
-            observer.invalid()
-        } else if (tieScenario()) {
-            observer.tie()
-        } else if (p1WinsScenario()) {
-            observer.p1Wins()
-        } else {
-            observer.p2Wins()
-        }
-    }
-
-    function p1WinsScenario() {
-        return p1Throw === THROW.ROCK && p2Throw === THROW.SCISSORS ||
-            p1Throw === THROW.SCISSORS && p2Throw === THROW.PAPER ||
-            p1Throw === THROW.PAPER && p2Throw === THROW.ROCK
-    }
-
-    function tieScenario() {
-        return p1Throw === p2Throw
-    }
-
-    function isInvalidThrow(playerThrow) {
-        return !VALID_THROWS.includes(playerThrow)
-    }
-
-    const THROW = {
-        ROCK: 'rock',
-        SCISSORS: 'scissors',
-        PAPER: 'paper'
-    }
-
-    const VALID_THROWS = [THROW.ROCK, THROW.SCISSORS, THROW.PAPER]
-}
+const {Requests} = require('../src/rps')
 
 describe('play', function () {
     let observer
